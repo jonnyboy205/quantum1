037e5e8197a75250e47f2323499244e04ac29507
Jonathan Bourdett <jab2279@columbia.edu>
2012-02-13 04:27:06 -0500

    calculated middle right

--- src/Exercise3.java
+++ src/Exercise3.java
@@ -1,6 +1,5 @@
 import java.util.ArrayList;
 
-
 public class Exercise3 {
 
 	private int j;
@@ -16,6 +15,50 @@ public class Exercise3 {
 	public int calculateProb(){
 		int ret = 0;
 		
+		ArrayList<int[]> leftSide = doLeft();
+		ArrayList<Gate> middle = doMiddle();
+//		ArrayList<int[]> rightSide = doRight();
+		String rightSideString = Integer.toBinaryString(m);
+		
+		ArrayList<Complex[]> middleRight = new ArrayList<Complex[]>();
+		for (int i=0; i<k; i++){
+			Complex[] element = (middle.get(i)).ket(rightSideString.charAt(i));
+			middleRight.set(i, element);
+		}
+		
+		return ret;
+	}
+
+//	private ArrayList<int[]> doRight() {
+//		//make m into binary
+//		String rightSideString = Integer.toBinaryString(m);
+//		
+//		//create right side of prob expression
+//		ArrayList<int[]> rightSide = new ArrayList<int[]>(k);
+//		for (int i=0; i<k; i++){
+//			if (rightSideString.charAt(i)=='0'){
+//				int[] temp = {1, 0};
+//				rightSide.set(i, temp);
+//			}
+//			else{ // leftSideString.charAt(0) == '1'
+//				int[] temp = {0, 1};
+//				rightSide.set(i, temp);
+//			}
+//		}
+//		return rightSide;
+//	}
+
+	private ArrayList<Gate> doMiddle() {
+		//create middle of prob expression
+		ArrayList<Gate> middle = new ArrayList<Gate>(k);
+		for (int x=0; x<k; x++){
+			middle.set(x, new Gate(x));
+		}
+		
+		return middle;
+	}
+
+	private ArrayList<int[]> doLeft() {
 		//first make j into binary
 		String leftSideString = Integer.toBinaryString(j);
 		
@@ -32,14 +75,7 @@ public class Exercise3 {
 			}
 		}
 		
-		//create middle of prob expression
-		ArrayList<Gate> middle = new ArrayList<Gate>(k);
-		for (int x=0; x<k; x++){
-			
-		}
-
-		
-		return ret;
+		return leftside;
 	}
 	
 }
